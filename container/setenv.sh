f005ae2ea1fc076e77e46b157ca32b151e7b61ed
Evan Lu <gardness@live.com>
2020-04-15 16:28:56 -0400

    Add database secret key to setenv.sh

--- container/setenv.sh
+++ container/setenv.sh
@@ -3,6 +3,7 @@ export JAVA_OPTS="$JAVA_OPTS -Ddatabase.dialect=org.hibernate.dialect.PostgreSQL
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.url=jdbc:postgresql://${DB_URL}:5432/${DB_NAME}"
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.user=${DB_USER}"
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.password=${DB_PASSWORD}"
+export JAVA_OPTS="$JAVA_OPTS -Ddatabase.secret.key=12345678"
 export JAVA_OPTS="$JAVA_OPTS -Dlogging.level.org.springframework=INFO"
 export JAVA_OPTS="$JAVA_OPTS -Dlogging.levelt.com.ascending=TRACE"
 export JAVA_OPTS="$JAVA_OPTS -Dserver.port=8080"
